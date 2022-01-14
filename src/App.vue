c928b5165d2c8e51b622d965e107dee1c51ba04a
GuYun-D <1946306647@qq.com>
2022-01-14 18:10:02 +0800

    feat: 集成vue-router和vuex

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <strong>{{ $store.state.name }}</strong>
+
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">狐妖</router-link>
+
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
-  },
 })
 </script>
 
